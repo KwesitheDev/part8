da83a4c14f889f150de29fcf4240857ea92c5f10
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-27 01:03:20 +0100

    bookCount and authorCount queries

--- firstPart/library-backend.js
+++ firstPart/library-backend.js
@@ -29,7 +29,9 @@ let authors = [
 
 /*
  * English:
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
+ * It might make more sense to associate a book with 
+ * its author by storing the author's id in the context
+ *  of the book instead of the author's name
  * However, for simplicity, we will store the author's name in connection with the book
 */
 
@@ -91,13 +93,15 @@ let books = [
 
 const typeDefs = `
   type Query {
-    dummy: Int
+    bookCount: Int!
+    authorCount: Int!
   }
 `
 
 const resolvers = {
   Query: {
-    dummy: () => 0
+    bookCount: ()=> books.length,
+    authorCount: () => authors.length
   }
 }
 
