69aee689f83ee5ce1f3a7de77938a485f69b5474
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-29 05:01:16 +0100

    setBirthyear

--- library-frontend/src/components/Authors.jsx
+++ library-frontend/src/components/Authors.jsx
@@ -1,12 +1,40 @@
+import { EDIT_AUTHOR } from "../mutations"
 import { ALL_AUTHORS } from "../queries"
-import { useQuery } from "@apollo/client"
+import { useMutation, useQuery } from "@apollo/client"
+import { useField } from "../hooks/useField"
 
 const Authors = () => {
-  const result = useQuery(ALL_AUTHORS)
+  const name = useField("text")
+  const born = useField("number")
 
-  if(result.loading) return <div>loading...</div>
+  const result = useQuery(ALL_AUTHORS)
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }]
+  })
+
+  if (result.loading) return <div>loading...</div>
   if (result.error) return <div>Error: {result.error.message}</div>
+
   const authors = result.data.allAuthors || []
+
+  const handleSubmit = async (event) => {
+    event.preventDefault()
+    if (!name.value || !born.value) return
+
+    try {
+      await editAuthor({
+        variables: {
+          name: name.value,
+          setBornTo: Number(born.value)
+        }
+      })
+
+      name.reset()
+      born.reset()
+    } catch (error) {
+      console.error("Error updating author:", error)
+    }
+  }
 
   return (
     <div>
@@ -27,6 +55,37 @@ const Authors = () => {
           ))}
         </tbody>
       </table>
+
+      <h3>set birthyear</h3>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name
+          <select
+            value={name.value}
+            onChange={name.onChange}
+            required
+          >
+            <option value="" disabled>
+              select author
+            </option>
+            {authors.map((a) => (
+              <option key={a.name} value={a.name}>
+                {a.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div>
+          born
+          <input
+            type="number"
+            value={born.value}
+            onChange={born.onChange}
+            required
+          />
+        </div>
+        <button type="submit">update author</button>
+      </form>
     </div>
   )
 }
