67a3c63e9f1af1e87a69e1c1454dbd9333445b46
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-29 03:53:17 +0100

    created client, added ApolloProvider and BrowserRouter

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { BrowserRouter } from "react-router-dom";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <BrowserRouter>
+      <App />
+      </BrowserRouter>
+  </ApolloProvider>
 );
