c6c01b096f51219e223855d7a0e8c2e97d5997f8
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-08-29 04:09:47 +0100

    addBook mutation

---
+++ library-frontend/src/mutations.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+export const ADD_BOOK = gql`
+    mutation AddBook(
+        $title: String!, 
+        $Author: String!, 
+        $published: Int!, 
+        $genres: [String!]!) {
+        addBook(
+            title: $title
+            author: $author
+            published: $published
+            genres: $genres
+        ) {
+            title
+            author
+            published
+            genres
+        }
+    }
+`
