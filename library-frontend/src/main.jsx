4e228ff984e9ea3b7d981b4e8f932f307f028780
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-06 23:22:04 +0100

    apollo client with auth header

--- library-frontend/src/main.jsx
+++ library-frontend/src/main.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink} from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
+import {setContext} from '@apollo/client/link/context'
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})  
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('library-user-token')
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null
+    }
+  }
+})
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
