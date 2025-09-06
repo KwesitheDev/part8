c4d42947e1887f15246f27bf8531d61f4124eddf
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-06 23:49:05 +0100

    added login route and logout button

--- library-frontend/src/components/Navigation.jsx
+++ library-frontend/src/components/Navigation.jsx
@@ -1,23 +1,31 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
-
-const Navigation = () => {
+const Navigation = ({ token, logout }) => {
   return (
-      <div>
+    <div>
+      <button>
+        <Link to="/">Home</Link>
+      </button>
+      <button>
+        <Link to="/authors">Authors</Link>
+      </button>
+      <button>
+        <Link to="/books">Books</Link>
+      </button>
+      {token ? (
+        <>
+          <button>
+            <Link to="/add">Add Book</Link>
+          </button>
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
         <button>
-            <Link to="/">Home</Link>
-          </button>
-          <button>
-            <Link to="/authors">Authors</Link>
-          </button>
-          <button>
-            <Link to="/books">Books</Link>
-          </button>
-          <button>
-            <Link to="/add">add Books</Link>
+          <Link to="/login">Login</Link>
         </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Navigation
+export default Navigation;
