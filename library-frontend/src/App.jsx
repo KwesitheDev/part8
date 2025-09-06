cbf971a936d8dd1eaf57d4ba7cbc561265df68b5
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-06 23:50:07 +0100

    logout, token, login route

--- library-frontend/src/App.jsx
+++ library-frontend/src/App.jsx
@@ -1,21 +1,30 @@
+import { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
-import { Routes, Route } from 'react-router-dom'
+import LoginForm from "./components/LoginForm";
+import { Routes, Route } from "react-router-dom";
 import Navigation from "./components/Navigation";
 
 const App = () => {
+  const [token, setToken] = useState(localStorage.getItem("library-user-token"));
+
+  const logout = () => {
+    setToken(null);
+    localStorage.removeItem("library-user-token");
+  };
 
   return (
     <div>
-      <Navigation />
+      <Navigation token={token} logout={logout} />
       <Routes>
         <Route path="/" element={<h2>Library App</h2>} />
         <Route path="/authors" element={<Authors />} />
         <Route path="/books" element={<Books />} />
-        <Route path="/add" element={<NewBook />} />
+        <Route path="/add" element={token ? <NewBook /> : <LoginForm setToken={setToken} />} />
+        <Route path="/login" element={<LoginForm setToken={setToken} />} />
       </Routes>
-      </div>
+    </div>
   );
 };
 
