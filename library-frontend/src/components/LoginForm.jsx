5b4d3c089f3f95ebd3a9becf786b5616b5456bbc
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-06 23:30:46 +0100

    Login Form

---
+++ library-frontend/src/components/LoginForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useMutation } from '@apollo/client'
+import { LOGIN } from '../queries'
+import { useField } from '../hooks/useField'
+
+const LoginForm = ({ setToken, show }) => {
+    const username = useField('text')
+    const password = useField('text')
+  const [login, {error}] = useMutation(LOGIN, {
+      onCompleted: (data) => {
+          const token = data.login.value
+          setToken(token)
+          localStorage.setItem('library-user-token', token)
+      }
+  })
+    if (!show) return null
+    const submit = async(e) => {
+        e.preventDefault()
+         await login({variables: {username: username.value, password: password.value}})
+        username.reset()
+        password.reset()
+    }
+
+    return (
+        <div>
+            <h2>Login</h2>
+            <form onSubmit={submit}>
+                <div>
+                    username <input {...username} />
+                </div>
+                <div>
+                    password <input {...password} />
+                </div>
+                <button type="submit">login</button>
+            </form>
+            {error && <p>{error.message}</p>}
+        </div>
+    )
+
+}
+
+export default LoginForm
